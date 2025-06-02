4f719142ba329a10f7285d21b0742bb7432c9d83
akshaychavan23031998 <akshayrchavan07@gmail.com>
2025-06-02 23:27:42 +0530

    Completed the Create Products API.

---
+++ backend/routes/productRoutes.js
@@ -0,0 +1,71 @@
+const express = require("express");
+const Product = require("../models/Product");
+const { protect, admin } = require("../middleware/authMiddleware");
+
+const router = express.Router();
+
+//@Route POST /api/products
+//@desc create a new products
+//@access private, only admins can create products.
+
+router.post("/", protect, admin, async (req, res) => {
+  try {
+    const {
+      name,
+      description,
+      price,
+      discountPrice,
+      countInStock,
+      brand,
+      category, // ✅ ADD THIS LINE
+      sizes,
+      colors,
+      collections,
+      material,
+      gender,
+      images,
+      isFeatured,
+      isPublished,
+      tags,
+      dimensions,
+      weight,
+      sku,
+    } = req.body;
+
+    const existingProduct = await Product.findOne({ sku });
+    if (existingProduct) {
+      return res.status(400).json({ message: "SKU already exists" });
+    }
+
+    const product = new Product({
+      name,
+      description,
+      price,
+      discountPrice,
+      countInStock,
+      brand,
+      category, // ✅ ADD THIS LINE
+      sizes,
+      colors,
+      collections,
+      material,
+      gender,
+      images,
+      isFeatured,
+      isPublished,
+      tags,
+      dimensions,
+      weight,
+      sku,
+      user: req.user._id, // refrance t the admin user who created the product.
+    });
+
+    const createdProduct = await product.save();
+    res.status(201).json(createdProduct);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Server Error");
+  }
+});
+
+module.exports = router;
