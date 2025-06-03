b7cf5b7085ef7a7da7b167f12dbfd8d3099731a0
akshaychavan23031998 <akshayrchavan07@gmail.com>
2025-06-03 23:48:36 +0530

    Completed Update API.

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -68,4 +68,77 @@ router.post("/", protect, admin, async (req, res) => {
   }
 });
 
+//@route PUT /api/product/:id
+//@desc update an existing product id
+//@access private, only admin can update or edit the existing products
+
+router.put("/:id", protect, admin, async (req, res) => {
+  try {
+    const {
+      name,
+      description,
+      price,
+      discountPrice,
+      countInStock,
+      brand,
+      category, // ✅ ADD THIS LINE
+      sizes,
+      colors,
+      collections,
+      material,
+      gender,
+      images,
+      isFeatured,
+      isPublished,
+      tags,
+      dimensions,
+      weight,
+      sku,
+    } = req.body;
+    // find product by ID
+    const product = await Product.findById(req.params.id);
+
+    if (product) {
+      // update product fileds
+      product.name = name || product.name;
+      product.description = description || product.description;
+      product.price = price || product.price;
+      product.discountPrice = discountPrice || product.discountPrice;
+      product.countInStock = countInStock || product.countInStock;
+      product.category = category || product.category;
+      product.brand = brand || product.brand;
+      product.sizes = sizes || product.sizes;
+      product.colors = colors || product.colors;
+      product.collections = collections || product.collections;
+      product.material = material || product.material;
+      product.gender = gender || product.gender;
+      product.images = images || product.images;
+      product.isFeatured =
+        isFeatured !== undefined ? isFeatured : product.isFeatured;
+      product.isPublished =
+        isPublished !== undefined ? isPublished : product.isPublished;
+      product.tags = tags || product.tags;
+      product.dimensions = dimensions || product.dimensions;
+      product.weight = weight || product.weight;
+      product.sku = sku || product.sku;
+
+      // save the updated product
+      const updatedProduct = await product.save();
+      res.json(updatedProduct);
+    } else {
+      res.status(404).json({ message: "Product Not Found" });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Server Error");
+  }
+});
+
+//@route DELETE /api/products/:id
+//@desc delete a product by id
+//@access private, its should be deleted by admin only.
+router.delete("/:id", protect, admin, async(req, res) => {
+  
+})
+
 module.exports = router;
