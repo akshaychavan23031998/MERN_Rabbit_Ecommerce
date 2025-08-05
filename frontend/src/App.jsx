103d8aa2719da373d7c8a29b290f347566a3587f
akshaychavan23031998 <akshayrchavan07@gmail.com>
2025-08-05 20:32:22 +0530

    screen freeze issue for mobile screens resolved.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -76,7 +76,7 @@
 // export default App;
 
 // App.js
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import UserLayout from "./components/Layout/UserLayout";
 import Privacy_Policy_Page from "./pages/Privacy_Policy_Page";
@@ -102,52 +102,74 @@ import OrderManagement from "./components/Admin/OrderManagement";
 import ProtectedRoutes from "./components/Common/ProtectedRoutes";
 
 const App = () => {
-  // Initialize LocomotiveScroll if needed
-  const locomotiveScroll = new LocomotiveScroll();
+  const scrollRef = useRef(null);
+
+  useEffect(() => {
+    const scroll = new LocomotiveScroll({
+      el: scrollRef.current,
+      smooth: true,
+      smartphone: {
+        smooth: true,
+      },
+      tablet: {
+        smooth: true,
+      },
+    });
+
+    return () => {
+      if (scroll) scroll.destroy();
+    };
+  }, []);
 
   return (
     <BrowserRouter
       future={{ v7_startTransition: true, v7_relativeSplatePath: true }}
     >
       <Toaster position="top-right" />
-      <Routes>
-        {/* User Layout */}
-        <Route path="/" element={<UserLayout />}>
-          <Route index element={<Home />} />
-          <Route path="login" element={<Login />} />
-          <Route path="register" element={<Register />} />
-          <Route path="profile" element={<Profile />} />
-          <Route path="collections/:collection" element={<CollectionPage />} />
-          <Route path="product/:id" element={<ProductDetails />} />
-          <Route path="checkout" element={<Checkout />} />
+      {/* This is the required scroll container for locomotive-scroll */}
+      <div data-scroll-container ref={scrollRef}>
+        <Routes>
+          {/* User Layout */}
+          <Route path="/" element={<UserLayout />}>
+            <Route index element={<Home />} />
+            <Route path="login" element={<Login />} />
+            <Route path="register" element={<Register />} />
+            <Route path="profile" element={<Profile />} />
+            <Route
+              path="collections/:collection"
+              element={<CollectionPage />}
+            />
+            <Route path="product/:id" element={<ProductDetails />} />
+            <Route path="checkout" element={<Checkout />} />
+            <Route
+              path="order-confirmation"
+              element={<OrderConfirmationPage />}
+            />
+            <Route path="order/:id" element={<OrderDetailsPage />} />
+            <Route path="my-orders" element={<MyOrderPage />} />
+          </Route>
+
+          {/* Admin Layout */}
           <Route
-            path="order-confirmation"
-            element={<OrderConfirmationPage />}
-          />
-          <Route path="order/:id" element={<OrderDetailsPage />} />
-          <Route path="my-orders" element={<MyOrderPage />} />
-        </Route>
+            path="/admin"
+            element={
+              <ProtectedRoutes role="admin">
+                <AdminLayout />
+              </ProtectedRoutes>
+            }
+          >
+            <Route index element={<AdminHomePage />} />
+            <Route path="users" element={<UserManagment />} />
+            <Route path="products" element={<ProductManagement />} />
+            <Route path="products/:id/edit" element={<EditProductPage />} />
+            <Route path="orders" element={<OrderManagement />} />
+          </Route>
 
-        {/* Admin Layout */}
-        <Route
-          path="/admin"
-          element={
-            <ProtectedRoutes role="admin">
-              <AdminLayout />
-            </ProtectedRoutes>
-          }
-        >
-          <Route index element={<AdminHomePage />} />
-          <Route path="users" element={<UserManagment />} />
-          <Route path="products" element={<ProductManagement />} />
-          <Route path="products/:id/edit" element={<EditProductPage />} />
-          <Route path="orders" element={<OrderManagement />} />
-        </Route>
-
-        {/* Policy pages */}
-        <Route path="/privacy-policy" element={<Privacy_Policy_Page />} />
-        <Route path="/terms" element={<Terms_of_Service_Page />} />
-      </Routes>
+          {/* Policy pages */}
+          <Route path="/privacy-policy" element={<Privacy_Policy_Page />} />
+          <Route path="/terms" element={<Terms_of_Service_Page />} />
+        </Routes>
+      </div>
     </BrowserRouter>
   );
 };
